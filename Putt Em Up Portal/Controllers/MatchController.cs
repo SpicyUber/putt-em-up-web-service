5a56415a1b6d3674d59a2560baa3498910a75c81
spicyuber <ilankovicaleksandar@gmail.com>
2025-11-24 15:28:30 +0100

    First commit

---
+++ Putt Em Up Portal/Controllers/MatchController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Putt_Em_Up_Portal.Models;
+
+namespace Putt_Em_Up_Portal.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MatchController : ControllerBase
+    {
+
+        [HttpGet(Name ="GetMatch")]
+      public IEnumerable<Match> Get(int id)
+        {
+           return Match.GetSampleList().Where((Match m) => { return m.MatchID == id; }).ToList<Match>();
+
+        }
+
+    }
+}
---
+++ Putt Em Up Portal/Models/Match.cs
@@ -0,0 +1,19 @@
+﻿namespace Putt_Em_Up_Portal.Models
+{
+    public class Match
+    {
+        public int MatchID { get; set; }
+        public DateTime StartDate { get; set; }
+
+        public bool Cancelled { get; set; }
+
+        public static IEnumerable<Match> GetSampleList()
+        {
+            LinkedList<Match> list = new LinkedList<Match>();
+            list.AddLast(new Match() { MatchID = 0, StartDate = new DateTime(2022, 6, 3) });
+            list.AddLast(new Match() { MatchID = 1, StartDate = new DateTime(2022, 7, 23) });
+            list.AddLast(new Match() { MatchID = 2, StartDate = new DateTime(2023, 2, 1) });
+            return list;
+        }
+    }
+}
