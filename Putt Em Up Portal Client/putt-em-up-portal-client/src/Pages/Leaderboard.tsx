d39509292801772dbf8b176444379b7b39f80799
spicyuber <ilankovicaleksandar@gmail.com>
2025-12-11 22:01:28 +0100

    Add placeholder main page (Leaderboard)

---
+++ Putt Em Up Portal Client/putt-em-up-portal-client/src/Pages/Leaderboard.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import PrimarySearchAppBar from "../Components/PrimarySearchAppBar";
+import { Box } from "@mui/material";
+
+export function Leaderboard(){
+
+    return(<><PrimarySearchAppBar/>
+    <Box>HIIII</Box>
+    
+    </>)
+}
